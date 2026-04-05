b37b4e28225e190883147e0a0effe3d144064741
tkdqn8066 <tkdqn8066@gmail.com>
2026-04-05 20:12:50 +0900

    3rd

---
+++ pages/[[...pageId]].js
@@ -0,0 +1,74 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import dynamic from 'next/dynamic'
+import { NotionAPI } from 'notion-client'
+import { NotionRenderer } from 'react-notion-x'
+
+const Code = dynamic(
+  () => import('react-notion-x/build/third-party/code').then(m => m.Code)
+)
+
+const Collection = dynamic(
+  () =>
+    import('react-notion-x/build/third-party/collection').then(
+      m => m.Collection
+    )
+)
+
+const Equation = dynamic(
+  () =>
+    import('react-notion-x/build/third-party/equation').then(m => m.Equation)
+)
+
+const Modal = dynamic(
+  () => import('react-notion-x/build/third-party/modal').then(m => m.Modal),
+  { ssr: false }
+)
+
+const ROOT_PAGE_ID = '2e7ac7e2940c804495f0dbd8ee2542b3'
+
+const notion = new NotionAPI()
+
+export default function Page({ recordMap }) {
+  return (
+    <>
+      <Head>
+        <title>Notion Embed</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <main className="page">
+        <NotionRenderer
+          recordMap={recordMap}
+          fullPage
+          darkMode={false}
+          nextLink={Link}
+          components={{
+            Code,
+            Collection,
+            Equation,
+            Modal
+          }}
+          mapPageUrl={pageId => `/${pageId}`}
+        />
+      </main>
+    </>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const pageId = params?.pageId?.[0] || ROOT_PAGE_ID
+  const recordMap = await notion.getPage(pageId)
+
+  return {
+    props: { recordMap },
+    revalidate: 60
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import 'react-notion-x/src/styles.css'
+import 'prismjs/themes/prism.css'
+import 'katex/dist/katex.min.css'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
